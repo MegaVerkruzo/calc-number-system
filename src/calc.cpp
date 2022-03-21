1590682952abdd80fe61b9f2a8ec3488aea16748
Grunskii Alexey <robochat@mail.ru>
2022-03-21 17:52:16 +0300

    First commit

---
+++ src/calc.cpp
@@ -0,0 +1,291 @@
+#include "calc.h"
+
+#include <cctype>   // for std::isspace
+#include <cmath>    // various math functions
+#include <iostream> // for error reporting via std::cerr
+
+namespace {
+
+const std::size_t max_decimal_digits = 10;
+
+enum class Op
+{
+    ERR,
+    SET,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    REM,
+    NEG,
+    POW,
+    SQRT
+};
+
+std::size_t arity(const Op op)
+{
+    switch (op) {
+    // error
+    case Op::ERR:
+        return 0;
+        // unary
+    case Op::NEG:
+        return 1;
+    case Op::SQRT:
+        return 1;
+        // binary
+    case Op::SET:
+        return 2;
+    case Op::ADD:
+        return 2;
+    case Op::SUB:
+        return 2;
+    case Op::MUL:
+        return 2;
+    case Op::DIV:
+        return 2;
+    case Op::REM:
+        return 2;
+    case Op::POW:
+        return 2;
+    }
+    return 0;
+}
+
+Op parse_op(const std::string & line, std::size_t & i)
+{
+    const auto rollback = [&i, &line](const std::size_t n) {
+        i -= n;
+        std::cerr << "Unknown operation " << line << std::endl;
+        return Op::ERR;
+    };
+    switch (line[i++]) {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+        --i; // a first digit is a part of op's argument
+        return Op::SET;
+    case '+':
+        return Op::ADD;
+    case '-':
+        return Op::SUB;
+    case '*':
+        return Op::MUL;
+    case '/':
+        return Op::DIV;
+    case '%':
+        return Op::REM;
+    case '_':
+        return Op::NEG;
+    case '^':
+        return Op::POW;
+    case 'S':
+        switch (line[i++]) {
+        case 'Q':
+            switch (line[i++]) {
+            case 'R':
+                switch (line[i++]) {
+                case 'T':
+                    return Op::SQRT;
+                default:
+                    return rollback(4);
+                }
+            default:
+                return rollback(3);
+            }
+        default:
+            return rollback(2);
+        }
+    default:
+        return rollback(1);
+    }
+}
+
+std::size_t skip_ws(const std::string & line, std::size_t i)
+{
+    while (i < line.size() && std::isspace(line[i])) {
+        ++i;
+    }
+    return i;
+}
+
+int parseNumberSystem(const std::string & line, std::size_t & i)
+{
+    if (line[i] == '0' && i + 1 < line.size()) {
+        if (std::tolower(line[i + 1]) == 'x' && i + 2 < line.size()) {
+            i += 2;
+            return 16;
+        }
+        else if (std::tolower(line[i + 1]) == 'b' && i + 2 < line.size()) {
+            i += 2;
+            return 2;
+        }
+        else if (line[i + 1] != '.') {
+            i++;
+            return 8;
+        }
+    }
+    return 10;
+}
+
+double parse_arg(const std::string & line, std::size_t & i)
+{
+    double res = 0;
+    std::size_t count = 0;
+    bool good = true;
+    bool integer = true;
+    double fraction = 1;
+    int numberSystem;
+    numberSystem = parseNumberSystem(line, i);
+    if (i < line.size() && line[i] == '.') {
+        good = false;
+    }
+    while (good && i < line.size() && count < max_decimal_digits) {
+        int decimalNumber = (std::isdigit(line[i]) ? line[i] - '0' : std::tolower(line[i]) - 'a' + 10);
+        switch (std::tolower(line[i])) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+        case 'f':
+            if (numberSystem <= decimalNumber) {
+                good = false;
+                break;
+            }
+            if (integer) {
+                res *= numberSystem;
+                res += decimalNumber;
+            }
+            else {
+                fraction /= numberSystem;
+                res += decimalNumber * fraction;
+            }
+            ++i;
+            ++count;
+            break;
+        case '.':
+            if (integer) {
+                integer = false;
+                ++i;
+                break;
+            }
+            else {
+                good = false;
+            }
+            break;
+        default:
+            good = false;
+            break;
+        }
+    }
+    if (!good) {
+        std::cerr << "Argument parsing error at " << i << ": '" << line.substr(i) << "'" << std::endl;
+    }
+    else if (i < line.size()) {
+        std::cerr << "Argument isn't fully parsed, suffix left: '" << line.substr(i) << "'" << std::endl;
+    }
+    return res;
+}
+
+double unary(const double current, const Op op)
+{
+    switch (op) {
+    case Op::NEG:
+        return -current;
+    case Op::SQRT:
+        if (current > 0) {
+            return std::sqrt(current);
+        }
+        else {
+            std::cerr << "Bad argument for SQRT: " << current << std::endl;
+            [[fallthrough]];
+        }
+    default:
+        return current;
+    }
+}
+
+double binary(const Op op, const double left, const double right)
+{
+    switch (op) {
+    case Op::SET:
+        return right;
+    case Op::ADD:
+        return left + right;
+    case Op::SUB:
+        return left - right;
+    case Op::MUL:
+        return left * right;
+    case Op::DIV:
+        if (right != 0) {
+            return left / right;
+        }
+        else {
+            std::cerr << "Bad right argument for division: " << right << std::endl;
+            return left;
+        }
+    case Op::REM:
+        if (right != 0) {
+            return std::fmod(left, right);
+        }
+        else {
+            std::cerr << "Bad right argument for remainder: " << right << std::endl;
+            return left;
+        }
+    case Op::POW:
+        return std::pow(left, right);
+    default:
+        return left;
+    }
+}
+
+} // anonymous namespace
+
+double process_line(const double current, const std::string & line)
+{
+    std::size_t i = 0;
+    const auto op = parse_op(line, i);
+    switch (arity(op)) {
+    case 2: {
+        i = skip_ws(line, i);
+        const auto old_i = i;
+        const auto arg = parse_arg(line, i);
+        if (i == old_i) {
+            std::cerr << "No argument for a binary operation" << std::endl;
+            break;
+        }
+        else if (i < line.size()) {
+            break;
+        }
+        return binary(op, current, arg);
+    }
+    case 1: {
+        if (i < line.size()) {
+            std::cerr << "Unexpected suffix for a unary operation: '" << line.substr(i) << "'" << std::endl;
+            break;
+        }
+        return unary(current, op);
+    }
+    default:
+        break;
+    }
+    return current;
+}
